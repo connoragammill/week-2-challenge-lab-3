8651be85be00f4bc04eec168aad57df719a18d39
conno <conno@CONSTRAPCUM>
2024-11-11 16:49:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace week_2_challenge_lab_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:"); 
+            int number = int.Parse(Console.ReadLine()); 
+            
+            Console.WriteLine("Enter the desired width:"); 
+            int width = int.Parse(Console.ReadLine());
+
+            for (int i = width; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(number);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
